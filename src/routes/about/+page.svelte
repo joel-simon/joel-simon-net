51a73ff98fd3bb467b7c7e5dacb5a34136c7d7e1
Joel Simon <joelsimon6@gmail.com>
2024-05-31 10:25:45 -0400

    update about page and convert to svelte

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,199 @@
+<div class="text_body">
+  <a id="back-button" href="/">
+    <img src="/imgs/icons/chevron-left.svg" />
+  </a>
+
+  <h1 class="text-3xl">About</h1>
+  <p class="mt-4">
+    I am a multidisciplinary artist / toolmaker / researcher who studied
+    computer science and art at Carnegie Mellon University before working on
+    bioinformatics at Rockefeller University. I am the founder / director of <a
+      href="https://www.morphogen.io/"
+      target="_blank">Morphogen</a
+    >, a generative design company developing
+    <a href="https://www.artbreeder.com/" target="_blank">Artbreeder</a>, a
+    massively collaborative creative tool and network. My works are somewhere in
+    the region between art, design and research and inspired by the systems of
+    biology computation and creativity.
+  </p>
+
+  <div class="flex flex-col md:flex-row mt-2s">
+    <div class="flex flex-col flex-1">
+      <h2 class="text-2xl">Interviews</h2>
+      <ul class="list-none list-inside">
+        <li>
+          2021 - <a
+            href="https://thegradientpub.substack.com/p/joel-simon-on-ai-art-and-artbreeder"
+            target="_blank"
+            >The Gradient
+          </a>
+        </li>
+        <li>
+          2020 - Video Profile by Science Friday - <a
+            href="https://youtu.be/oP4nK61CcbA"
+            target="_blank">Building Beauty with Biology</a
+          >
+        </li>
+        <li>
+          2020 - <a href="https://francovarriano.com/techart" target="_blank"
+            >Tech+Art Podcast</a
+          >
+        </li>
+        <li>
+          2020 - <a
+            href="https://soundcloud.com/ieeeras-softrobotics/joel-simon-art-machine-intelligence-and-soft-robotics-design"
+            target="_blank">IEEE Soft Robotics Podcast</a
+          >
+        </li>
+        <li>
+          2019 - <a
+            href="https://www.dezeen.com/2019/01/03/evolving-floor-plans-research-joel-simon/"
+            target="_blank">Dezeen - Evolving Floor Plans</a
+          >
+        </li>
+        <li>
+          2017 - <a
+            href="https://www.labocine.com/spotlight/146"
+            target="_blank">Labocine - Ecology Modeling</a
+          >
+        </li>
+        <li>
+          2015 - <a
+            href="https://creators.vice.com/en_us/article/78exwy/now-you-can-graffiti-your-friends-facebook-walls"
+            target="_blank">Creators Projects - Facebook Graffiti</a
+          >
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex flex-col flex-1">
+      <h2 class="text-2xl">Exhibits</h2>
+      <ul class="list-none list-inside">
+        <li>
+          2024 - 120710 Gallery - <a
+            href="http://www.aiartonline.com/"
+            target="_blank"
+          >
+            https://www.curatingai.art/</a
+          >
+        </li>
+        <li>
+          2020 - <a href="http://www.aiartonline.com/" target="_blank"
+            >NeurIPS Workshop on ML for Creativity and Design</a
+          >
+        </li>
+        <li>
+          2020 - <a
+            href="https://www.arebyte.com/real-time-constraints"
+            target="_blank">Arebyte Gallery</a
+          >
+        </li>
+        <li>2019 - Ars Electronica</li>
+        <li>2019 - NeurIPS Workshop on ML for Creativity and Design</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="flex flex-col md:flex-row mt-2">
+    <div class="flex flex-col flex-1">
+      <h2 class="text-2xl">Invited Talks</h2>
+      <ul class="list-none list-inside">
+        <li>2023 - Acadia Special Topics Panel</li>
+        <li>
+          2021 - <a
+            href="https://slideslive.com/38969309/from-technologies-to-tools?ref=account-folder-92044-folders"
+            target="_blank">NeurIPS - ML for Creativity and Design</a
+          >
+        </li>
+        <li>
+          2020 - <a
+            href="https://ali-design.github.io/deepcreativity/"
+            target="_blank"
+            >MIT 6.S192: Deep Learning for Art, Aesthetics, and Creativity</a
+          >
+          -
+          <a href="https://youtu.be/I0V2UTKIei0" target="_blank">Youtube</a>
+        </li>
+        <li>
+          2019 - Bay Area Maker Faire - <em
+            >Augmenting Creativity With Massively Collaborative Creative Tools</em
+          >
+        </li>
+        <li>
+          2019 - <a href="https://www.recessart.org/" target="_blank"
+            >Recess Art</a
+          >
+          - <em>Biologically Inspired</em>
+        </li>
+        <li>2019 - Uber AI Labs</li>
+        <li>
+          2019 - Rutgers University - <a
+            href="https://www.youtube.com/watch?v=8L1bNz4YYjg"
+            target="_blank">Computational / Biological Design</a
+          >
+        </li>
+        <li>2019 - Adobe AI Labs</li>
+      </ul>
+    </div>
+
+    <div class="flex flex-col flex-1">
+      <h2 class="text-2xl">Publications</h2>
+      <ul class="list-none list-inside">
+        <li>
+          2021 - <a
+            href="https://www.morphogen.io/research/sculpting_with_words.pdf"
+            target="_blank">Sculpting with Words</a
+          >
+        </li>
+        <li>
+          2021 - <a
+            href="https://pure.itu.dk/ws/files/86612620/cc_rafner2021.pdf"
+            target="_blank"
+            >Utopian or Dystopian?: using a ML-assisted image generation game to
+            empower the general public to envision the future</a
+          >
+        </li>
+        <li>
+          2020 - <a
+            href="https://dl.acm.org/doi/abs/10.1145/3377929.3398156"
+            target="_blank"
+            >Antimander: open source detection of gerrymandering though
+            multi-objective evolutionary algorithms</a
+          >
+        </li>
+        <li>
+          2020 - <a href="https://arxiv.org/pdf/2010.12324.pdf" target="_blank"
+            >ML-assisted image generation to engage the crowd in complex
+            socioscientific problems</a
+          >
+        </li>
+        <li>
+          2020 - <a
+            href="https://www.mdpi.com/2071-1050/12/11/4393"
+            target="_blank">Self-Organizing Floor Plans in Care Homes</a
+          >
+        </li>
+        <li>
+          2018 - <a
+            href="https://www.sciencedirect.com/science/article/abs/pii/S0022175918303028?via%3Dihub"
+            target="_blank"
+            >Sequencing the peripheral blood B and T cell repertoire –
+            Quantifying robustness and limitations</a
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <br />
+  <img src="/imgs/lamp_me2.jpg" width="600" alt="Lamp" />
+</div>
+
+<style>
+  li {
+    text-align: left;
+    /* Hanging indent! */
+    margin-left: 3em;
+    text-indent: -3em;
+  }
+</style>
